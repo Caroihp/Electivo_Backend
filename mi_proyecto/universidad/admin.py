d3c15fb568f6e440b0a868c1d5b5c731b734af14
Caro.ihp <carolina.hp251014@gmail.com>
2025-04-07 23:40:18 -0400

    Actividad 1 Terminada

--- mi_proyecto/universidad/admin.py
+++ mi_proyecto/universidad/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Universidad, Curso, Estudiante, Matricula
 
 # Register your models here.
+
+admin.site.register(Universidad)
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Matricula)
